11fe23ef80154846dd304a97744e49d2ce58d404
Derek Neuts <dcneuts@gmail.com>
2017-12-17 00:58:36 -0800

    Added project features section, used custom post types and custom fields, used loops for multiple columns of information

--- wp-content/themes/bootstrap2wordpress/page-home.php
+++ wp-content/themes/bootstrap2wordpress/page-home.php
@@ -32,6 +32,9 @@ $features_section_image = get_field('features_section_image');
 $features_section_title = get_field('features_section_title');
 $features_section_body  = get_field('features_section_body');
 
+$project_feature_title = get_field('project_feature_title');
+$project_feature_body  = get_field('project_feature_body');
+
 get_header(); ?>
 
     <!-- HERO -->
@@ -194,26 +197,36 @@ get_header(); ?>
     <section id="project-features">
         <div class="container">
 
-            <h2>Final Project Features</h2>
-            <p class="lead">Throughout this entire course, you work towards building an incredibly beautiful website. Want to see the website <strong>you</strong> are going to build? <em>You're looking at it!</em> The website you're using right now is the website you will have built entirely by yourself, by the end of this course.
-            </p>
+            <h2><?php echo $project_feature_title; ?></h2>
+            <p class="lead"><?php echo $project_feature_body; ?></p>
 
             <div class="row">
-                <div class="col-sm-4">
-                    <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/icon-design.png" alt="Design">
-                    <h3>Sexy &amp; Modern Design</h3>
-                    <p>You get to work with a modern, professional quality design &amp; layout.</p>
-                </div><!-- col -->
-                <div class="col-sm-4">
-                    <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/icon-code.png" alt="Code">
-                    <h3>Quality HTML5 &amp; CSS3</h3>
-                    <p>You'll learn how hand-craft a stunning website with valid, semantic and beautiful HTML5 & CSS3.</p>
-                </div><!-- col -->
-                <div class="col-sm-4">
-                    <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/icon-cms.png" alt="Design">
-                    <h3>Easy-to-use CMS</h3>
-                    <p>Allow your clients to easily update their websites by converting your static websites to dynamic websites, using WordPress.</p>
-                </div><!-- col -->
+
+	            <!-- Loop using WP query using custom feature post type -->
+                <?php $loop = new WP_Query(array('post_type' => 'project_feature', 'orderby' => 'post_id', 'order' =>
+		                'ASC')
+                ); ?>
+
+                <?php while($loop->have_posts()) : $loop->the_post(); ?>
+
+		            <!-- Look to grab all icons and titles corresponding to post_type => course_feature above -->
+
+	                <div class="col-sm-4">
+
+		                <!-- If user inserts image in backend custom fields, then display that here -->
+		                <!-- Pulls from project_feature defined on line 206 -->
+		                <?php
+		                    if(has_post_thumbnail()) {
+		                    	the_post_thumbnail();
+		                    }
+		                ?>
+
+		                <h3><?php the_title(); ?></h3>
+		                <p><?php the_content(); ?></p>
+	                </div><!-- col -->
+
+                <?php endwhile; ?>
+
             </div><!-- row -->
 
         </div><!-- container -->
