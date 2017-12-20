f97f0ffa62cfb5b03a5f9fea4787d98d8353e2f6
Derek Neuts <dcneuts@gmail.com>
2017-12-19 23:28:52 -0800

    Began editing blog post meta data section, not yet finished, pushing to repo to preserve edits.

--- wp-content/themes/bootstrap2wordpress/template-parts/content.php
+++ wp-content/themes/bootstrap2wordpress/template-parts/content.php
@@ -13,17 +13,27 @@
 	<header class="entry-header">
 		<?php
 		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
+			the_title( '<h3 class="entry-title">', '</h3>' );
 		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+			the_title( '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' );
 		endif;
 
 		if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php bootstrap2wordpress_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php
-		endif; ?>
+
+			<div class="post-details">
+				<i class="fa fa-user"></i> <?php the_author(); ?>
+				<i class="fa fa-clock-o"></i> <time><?php the_date(); ?></time>
+				<i class="fa fa-folder-open"></i> <?php the_category(', '); ?>
+				<i class="fa fa-tags"></i> Tagged <?php the_tags('', ', ', ''); ?>
+				<div class="post-comments-badge">
+					<a href=""><i class="fa fa-comments"></i> <?php comments_number(0,1,'%'); ?>></a>
+				</div><!-- post-comments-badge -->
+
+				<div><<?php edit_post_link('Edit', '<div><i class="fa fa-pencil"></i>', '</div>' ); ?></div>
+
+			</div><!-- post details -->
+
+		<?php endif; ?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
