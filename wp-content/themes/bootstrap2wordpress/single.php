0ada73f1b079e959864901e78fed708eb97c90b5
Derek Neuts <dcneuts@gmail.com>
2017-12-27 22:58:44 -0800

    Corrected version before major modifications to make blog post look like rest of theme

--- wp-content/themes/bootstrap2wordpress/single.php
+++ wp-content/themes/bootstrap2wordpress/single.php
@@ -10,28 +10,27 @@
 get_header(); ?>
 
 <div class="container"><!-- closed further down -->
-	<div class="row" id="primary"><!-- closed further down -->
+	<div class="row" id="primary">
 
-	                              <!-- BLOG CONTENT -->
 		<main id="content" class="col-sm-8">
 
-            <?php while (have_posts()) : the_post();
+            <?php while (have_posts()) : the_post(); ?>
 
-                get_template_part('template-parts/content', get_post_type());
+                <?php get_template_part('template-parts/content', 'page'); ?>
 
-                the_post_navigation();
+                <?php the_post_navigation(); ?>
 
+				<?php
                 // If comments are open or we have at least one comment, load up the comment template.
                 if (comments_open() || get_comments_number()) :
                     comments_template();
                 endif;
-
-            endwhile; // End of the loop.
-            ?>
+				?>
+            <?php endwhile; // End of the loop. ?>
 
 		</main><!-- #content -->
 
-	                              <!-- SIDEBAR -->
+	    <!-- SIDEBAR -->
 		<aside class="col-sm-4">
             <?php get_sidebar(); ?>
 		</aside>
